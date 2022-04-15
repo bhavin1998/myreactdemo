d034efa46643f45df0ae21765a24f439f404fe64
bhavin1998 <bhavingediya123@gmail.com>
2022-04-15 16:34:10 +0530

    Bootstrap library added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let a = 10;
+ 
   return (
     <>
-    <h1>This is first demo</h1>
-    <nav>
-      <li>list 1</li>
-      <li>list 2</li>
-      <li>list 3</li>
-    </nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="/">First App</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-    <div className="container">
-      <h2> value of A is {a}</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde autem repellendus a expedita eius soluta magnam voluptas molestiae laudantium nobis suscipit quasi neque itaque, vel corporis praesentium ipsa eos?</p>
-      </div>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+      </ul>
+      <form className="form-inline my-2 my-lg-0">
+        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
     </>
   );
 }
